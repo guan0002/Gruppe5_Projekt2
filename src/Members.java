72960f5db0af04b196fe9daf2718597efb8e6049
AdamLevy2001 <adle0001@stud.ek.dk>
2025-11-24 12:47:15 +0100

    register method

--- src/Members.java
+++ src/Members.java
@@ -4,18 +4,24 @@ public class Members {
     String name;
     int age;
     String memberType;
-    String activityType;
 
-    public Members(String name, int age, String memberType, String activityType){
+    public Members(String name, int age, String memberType){
         this.name=name;
         this.age=age;
         this.memberType=memberType;
-        this.activityType=activityType;
 
     }
     public static void registerMember(){
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Write member name");
+        String name = input.nextLine();
+
+        System.out.println("Write age");
+        int age = input.nextInt();           // Vi laver senere en loop
+
+        System.out.println("Write membertype \n Junior Member \n Senior Member \n Retired Member \n Passiv Member");
+        String memberType = input.nextLine();
     }
 }
 
