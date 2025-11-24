7bb1a25e4d0106420dc95d5e52036c38caf9f1c1
AdamLevy2001 <adle0001@stud.ek.dk>
2025-11-24 11:02:48 +0100

    blabla

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,7 @@ public class Main {
     public void testMetode() {
         System.out.println("Dette er en test 🐬🍆💦");
     }
+    public void adamMetode(){
+        System.out.println("blabla");
+    }
 }
