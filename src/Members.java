14d9137931b67d3441db12dd2ef5f944294f7879
AdamLevy2001 <adle0001@stud.ek.dk>
2025-11-24 12:32:10 +0100

    Members Classe

---
+++ src/Members.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Members {
+    String name;
+    int age;
+    String memberType;
+    String activityType;
+
+    public Members(String name, int age, String memberType, String activityType){
+        this.name=name;
+        this.age=age;
+        this.memberType=memberType;
+        this.activityType=activityType;
+
+    }
+    public static void registerMember(){
+        Scanner input = new Scanner(System.in);
+
+    }
+}
+
