048f6da44859fd8068677a4f20126ca0ed49f50f
maho0012 <maho0012@stud.ek.dk>
2025-11-24 10:53:37 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public void testMetode() {
+        System.out.println("Dette er en test 🐬🍆💦");
+    }
 }
