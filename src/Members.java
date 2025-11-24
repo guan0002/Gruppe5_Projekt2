8e02c278abffb30467d618b1d7bedd41913c7884
maho0012 <maho0012@stud.ek.dk>
2025-11-24 13:27:23 +0100

    RegisterMember motoden, arbejdet videre på den ved at implementere try and catch og if else statement på memberType indtastning.

--- src/Members.java
+++ src/Members.java
@@ -1,27 +1,54 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Members {
     String name;
     int age;
     String memberType;
+    String paid;
 
-    public Members(String name, int age, String memberType){
-        this.name=name;
-        this.age=age;
-        this.memberType=memberType;
+    public Members(String name, int age, String memberType, String paid) {
+        this.name = name;
+        this.age = age;
+        this.memberType = memberType;
+        this.paid=paid;
 
     }
-    public static void registerMember(){
+
+    public void registerMember() {
         Scanner input = new Scanner(System.in);
+        boolean correctIntFormat = false;
+        boolean correctMemberType = false;
 
         System.out.println("Write member name");
         String name = input.nextLine();
 
-        System.out.println("Write age");
-        int age = input.nextInt();           // Vi laver senere en loop
+        while (!correctIntFormat) {
+            System.out.println("Write age");
+            try {
+               int age = input.nextInt();           // Vi laver senere en loop
+                correctIntFormat = true;
+                input.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("We only accept numbers, not letters. Try again!");
+                input.nextLine();
+            }
+        }
 
-        System.out.println("Write membertype \n Junior Member \n Senior Member \n Retired Member \n Passiv Member");
-        String memberType = input.nextLine();
+        while (!correctMemberType) {
+            System.out.println("Write membertype: \n Junior Member \n Senior Member \n Retired Member \n Passive Member");
+            String memberType = input.nextLine();
+            if (memberType.equalsIgnoreCase("Junior Member") || memberType.equalsIgnoreCase("Senior Member") || memberType.equalsIgnoreCase("Retired Member") || memberType.equalsIgnoreCase("Passive Member")) {
+                correctMemberType=true;
+            }
+            else {
+                System.out.println("👇You have to choose one of the Member types that are offered below👇");
+            }
+        }
+
+
+
+        Members m1 = new Members(name, age, memberType, paid);
     }
 }
 
