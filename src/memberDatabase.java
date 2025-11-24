1ae65340a7a844f5704d27d760a3dc861ec043e2
MogensMickisen <mimo0014@stud.ek.dk>
2025-11-24 12:46:46 +0100

    0.1 In process of rewriting and applying code from older list

--- src/memberDatabase.java
+++ src/memberDatabase.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class memberDatabase {
     public static ArrayList<member> loadDatabase() throws IOException {
         ArrayList<member> members = new ArrayList<>(); //TBD
 
-        FileReader cdb = new FileReader("src/memberDatabase.txt");//Tilgår customerDatabase.txt
+        FileReader cdb = new FileReader("src/memberDatabase.txt");//Tilgår memberDatabase.txt
         BufferedReader load = new BufferedReader(cdb);
 
         String line = load.readLine();
@@ -18,9 +16,47 @@ public class memberDatabase {
                 line = load.readLine();
                 continue; // hopper over den forkerte linje
             }
-            String name = entry[0]; //TBD
-            String memberType = entry[1]; //TBD
-            String paid = entry[2]; //TBD
+            String name         = entry [0];
+            String age          = entry [1];
+            String memberType   = entry [2];
+            String paid         = entry [3]; //TBD
+
+            member m = new member(name, email, date, time);
+            if (entry.length > 4) {
+                try {
+                    m.lastPayment = Double.parseDouble(entry[4]);
+                } catch (Exception e) {
+                    m.lastPayment = 0;
+                }
+
+                if (entry.length > 5) {
+                    m.lastHaircutType = entry[5];
+                }
+
+                if (entry.length > 6) {
+                    String productString = entry[6];
+                    if (!productString.isEmpty()) {
+                        m.lastProducts = productString.split(";");
+                    }
+                }
+            }
+            members.add(m);
+
+            line = load.readLine();
+        }
+        load.close();
+        return members;
         }
     }
+//opdaterer customerDatabase.txt med ny kunde
+public static void saveCustomersToFile(ArrayList<Customer> customers) throws IOException {
+
+    FileWriter file = new FileWriter("src/customerDatabase.txt", false);
+    PrintWriter out = new PrintWriter(file);
+
+    for (Customer c : customers) {
+        out.println(c.toString());
+    }
+
+    out.close();
 }
