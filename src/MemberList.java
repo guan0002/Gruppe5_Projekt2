b5078350d6e08e40c0a86b5138ab12caac197b8a
guan0002 <guan0002@stud.ek.dk>
2025-11-26 12:23:13 +0100

    her er en fix til memberlist det var så lidt

--- src/MemberList.java
+++ src/MemberList.java
@@ -4,18 +4,17 @@ public class MemberList {
 
     public static void show() {
 
-        Scanner input = MainMenu.scanner;
-
         System.out.println("Member List:");
 
         for (Members m : Members.membersRegister) {
             System.out.println(m);
         }
 
-        System.out.println("\nPress 0 to go back to the menu.");
+        int choice = InputValidation.ReadInt("\nPress 0 to go back to the menu.");
 
-        int choice = input.nextInt();
-        input.nextLine();
+        while (choice != 0) {
+            choice = InputValidation.ReadInt("Press 0 to go back to the menu.");
+        }
 
     }
 }
